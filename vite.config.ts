7ff64595e2977271e4607c0e7dfae3251bc1214f
Cristian Villalobos <cristianvillalobos666@gmail.com>
2025-01-22 17:02:41 -0300

    Agrega vite-plugin-image-optimizer y configura la optimización de imágenes en vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    ViteImageOptimizer({
+      png: {
+        quality: [0.6, 0.8], // Compresión para PNG
+      },
+      jpeg: {
+        quality: 80, // Calidad del JPEG (0-100)
+      },
+      webp: {
+        lossless: true, // Habilita compresión sin pérdida para WebP
+      },
+      svg: {
+        multipass: true, // Optimización múltiple para SVG
+      },
+      gif: {
+        optimizationLevel: 3, // Nivel de optimización para GIF
+      },
+      jpg: {
+        quality: 80, // Calidad del JPG (0-100)
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
